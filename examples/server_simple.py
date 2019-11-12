cd81a63ecbd0ece8acf8933effef8fd7fdf2bf34
codeif <me@codeif.com>
2019-11-12 15:37:33 +0800

    First commit

---
+++ examples/server_simple.py
@@ -0,0 +1,13 @@
+# server_simple.py
+from aiohttp import web
+from aiohttp_bam import bam_factory
+
+
+async def handle(request):
+    return web.Response(text="Hello")
+
+
+app = web.Application(middlewares=[bam_factory('your username', 'your password')])
+app.add_routes([web.get("/", handle)])
+
+web.run_app(app)
